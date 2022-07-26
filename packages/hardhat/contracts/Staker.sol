ae43534033e01f63abf427ade144c96c73739071
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-07-26 12:50:10 +0700

    Implement stake function and define constants

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -3,18 +3,24 @@ pragma solidity ^ 0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 contract Staker {
 
-  ExampleExternalContract public exampleExternalContract;
+  mapping ( address => uint256 ) public balances;
+  uint public constant threshold = 1;
+  uint public constant deadline = 2;
 
   constructor(address exampleExternalContractAddress) {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
+      
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-
+  function stake(uint _stake) public {
+    _burn(msg.sender, _stake);
+    balances[msg.sender] += _stake;
+  }
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
