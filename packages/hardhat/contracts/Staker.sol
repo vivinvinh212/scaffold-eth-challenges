7970314ec584e0ac698d1d2d093d00d106e686d3
vivinvinh212 <83176944+vivinvinh212@users.noreply.github.com>
2022-07-26 12:56:26 +0700

    Finish implementing stake function

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -5,21 +5,44 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract Staker {
+/**
+* @title Contract for staking
+* @author scaffold-eth
+* @notice Contract allowing user to stake ETH and receive staking rewards
+*/
 
+contract Staker {
+  // External contract that will old stacked funds
+  ExampleExternalContract public exampleExternalContract;
+
+  // Store balances of stakers through addr mapping
   mapping ( address => uint256 ) public balances;
-  uint public constant threshold = 1;
+
+  // Stake threshold (eth)
+  uint public constant threshold = 1 ether;
+
+  // Stake deadline (days)
   uint public constant deadline = 2;
 
+  // Contract's Events
+  event Stake(address indexed sender, uint256 amount);
+
+  /**
+  * @notice Constructor
+  * @param exampleExternalContractAddress Address of the external contract that will hold staked funds
+  */
   constructor(address exampleExternalContractAddress) {
       
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
-  function stake(uint _stake) public {
-    _burn(msg.sender, _stake);
-    balances[msg.sender] += _stake;
+  /**
+  * @notice Stake function to update staker balances by the addition of their staked amount
+  */
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
   }
 
   // After some `deadline` allow anyone to call an `execute()` function
